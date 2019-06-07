470ec239c53163b5e67c38575569b7376af87e36
whackney <hackney.w@husky.neu.edu>
2019-06-07 10:42:01 -0400

    Update AShape.java

--- src/animator_model/shape/AShape.java
+++ src/animator_model/shape/AShape.java
@@ -17,22 +17,26 @@ public class AShape implements IShape {
   private int red;
   private int green;
   private int blue;
-  private IMotion motion;
 
   /**
-   * Base constructor that takes in all necessary values to create a shape.
-   * @param x the x position in the frame
-   * @param y the y position in the frame
-   * @param w the general width of the shape
-   * @param h the general height of the shape
-   * @param r the red value for it's color 0 - 255
-   * @param g the green value for it's color 0 - 255
-   * @param b the blue value for it's color 0 - 255
+   * Constructor to create a shape. Invariants are w and h cannot be less than 0, rgb cannot
+   * be less than 0 or greater than 255.
+   * @param shapeID name of shape
+   * @param shapeType shape type
+   * @param x x value of center of shape
+   * @param y y value of center of shape
+   * @param w width of shape
+   * @param h height of shape
+   * @param r red value of shape
+   * @param g green value of shape
+   * @param b blue value of shape
    */
-  public AShape(String shapeID, ShapeType shapeType, int x, int y, int r, int w, int h, int g,
-                int b, IMotion motion) {
-    if (w < 0 || h < 0 || r < 0 || g < 0 || b < 0) {
-      throw new IllegalArgumentException("Width and height and/or rgb must be positive.");
+  public AShape(String shapeID, ShapeType shapeType, int x, int y, int w, int h, int r, int g,
+                int b) {
+    if (w < 0 || h < 0 || r < 0 || g < 0 || b < 0 || shapeID == null || shapeType == null
+            || r > 255 || g > 255 || b > 255) {
+      throw new IllegalArgumentException("Width, height and/or rgb must be positive and less than"
+              + " 256. Shape type and shapeID cannot be null.");
     }
     this.shapeID = shapeID;
     this.shapeType = shapeType;
@@ -43,49 +47,66 @@ public class AShape implements IShape {
     this.red = r;
     this.green = g;
     this.blue = b;
-    this.motion = motion;
   }
 
-  //TODO: probably a fuckton of getters and most of our methods should be in here
   @Override
   public String getShapeID() {
     return this.shapeID;
   }
 
+  @Override
   public ShapeType getShapeType() {
     return this.shapeType;
   }
 
+  @Override
   public int getxPos() {
     return this.xPos;
   }
 
+  @Override
   public int getyPos() {
     return this.yPos;
   }
 
+  @Override
   public int getWidth() {
     return this.width;
   }
 
+  @Override
   public int getHeight() {
     return this.height;
   }
 
+  @Override
   public int getRed() {
     return this.red;
   }
 
+  @Override
   public int getGreen() {
     return this.green;
   }
 
+  @Override
   public int getBlue() {
     return this.blue;
   }
 
-  public IMotion getMotion() {
-    return this.motion;
+  @Override
+  public String getShapeTypeAsString() {
+    switch (this.shapeType) {
+      case RECTANGLE:
+        return "rectangle";
+      case ELLIPSE:
+        return "ellipse";
+      case SQUARE:
+        return "square";
+      case CIRCLE:
+        return "circle";
+      default:
+        throw new IllegalStateException("If you see this error, something has gone very wrong.");
+    }
   }
 }
-
