ccb903ca8097364a34c6eb80f462d5318134e604
whackney <hackney.w@husky.neu.edu>
2019-06-07 00:40:14 -0400

    Update IMotion.java

--- src/animator_model/motion/IMotion.java
+++ src/animator_model/motion/IMotion.java
@@ -1,7 +1,19 @@
 package animator_model.motion;
 
+import animator_model.shape.IShape;
+
 /**
  * Interface represents a motion of a shape.
  */
 public interface IMotion {
+  String getTextOutput();
+
+  int getTStart();
+
+  int getTEnd();
+
+  IShape getShape();
+
+  void interpolate();
 }
+
