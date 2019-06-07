e837a0dcd1b424fba65214db00887d7f60dc38b3
whackney <hackney.w@husky.neu.edu>
2019-06-07 16:35:08 -0400

    Added sorting

--- src/animator_model/AnimatorModelImpl.java
+++ src/animator_model/AnimatorModelImpl.java
@@ -57,8 +57,8 @@ public class AnimatorModelImpl implements IAnimatorModel {
       throw new IllegalArgumentException("Tick must be a positive integer.");
     }
 
-    for (int motion = 0; motion < moveList.size(); motion++) {
-      IMotion currentMove = moveList.get(motion);
+    for (int i = 0; i < moveList.size(); i++) {
+      IMotion currentMove = moveList.get(i);
       if (currentMove.getTStart() <= tick && currentMove.getTEnd() >= tick) {
         currentMove.interpolate(tick);
         shapes.add(currentMove.getShape());
@@ -91,25 +91,63 @@ public class AnimatorModelImpl implements IAnimatorModel {
   }
 
   private void sortMoveList() {
+    ArrayList<String> keys = new ArrayList<>();
     for (IMotion motion: moveList) {
       IShape currentShape = motion.getShape();
       String key = currentShape.getShapeID();
 
+      // This is to add a new key to the hashmap
       if (sortedMoveList.get(key) == null) {
         sortedMoveList.put(key, new ArrayList<IMotion>());
+        keys.add(key);
       }
 
+      //This is to add a starting motion to a specific key
       if (sortedMoveList.get(key).isEmpty()) {
         sortedMoveList.get(key).add(motion);
       }
 
-      int size = sortedMoveList.size();
-      for (int i = 0; i < size; i++) {
-        IMotion originalMotion = sortedMoveList.get(key).get(i);
-        if (motion.getTEnd() == originalMotion.getTStart()) {
-          moveList.add(motion);
+      else {
+        int size = sortedMoveList.get(key).size();
+
+        for (int i = 0; i < size; i++) {
+          int motionStartTime = motion.getTStart();
+          int motionEndTime = motion.getTEnd();
+          int indexedMotionStart = sortedMoveList.get(key).get(i).getTStart();
+          int indexedMotionEnd = sortedMoveList.get(key).get(i).getTEnd();
+          if ((motionStartTime >= indexedMotionStart && motionStartTime < indexedMotionEnd)
+                  || (motionEndTime > indexedMotionStart && motionEndTime <= indexedMotionEnd)
+                  || (motionStartTime <= indexedMotionStart && motionEndTime >= indexedMotionEnd)) {
+            throw new IllegalArgumentException("Overlapping moves for same shape");
+          }
+
+          sortedMoveList.get(key).add(motion);
         }
       }
     }
+
+    for (String key: keys) {
+      sortedMoveList.put(key, this.bubbleSort(sortedMoveList.get(key)));
+    }
+  }
+
+  private ArrayList<IMotion> bubbleSort(ArrayList<IMotion> list) {
+    int size = list.size();
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        IMotion currentMotion = list.get(i);
+        IMotion checkingMotion = list.get(j);
+        if (currentMotion.getTStart() > checkingMotion.getTStart()) {
+          list.add(j, currentMotion);
+          list.add(i, checkingMotion);
+        }
+      }
+    }
+
+    return list;
+  }
+
+  private Boolean isInSequence() {
+    return null;
   }
 }
