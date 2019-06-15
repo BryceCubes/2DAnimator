2dac4b3121985704e71acfe398776b55713c39e7
whackney <hackney.w@husky.neu.edu>
2019-06-15 17:56:07 -0400

    changed declareshape

--- src/cs3500/animator/model/AnimatorModelImpl.java
+++ src/cs3500/animator/model/AnimatorModelImpl.java
@@ -79,8 +79,10 @@ public class AnimatorModelImpl implements IAnimatorModel {
     @Override
     public AnimationBuilder<IAnimatorModel> declareShape(String name, String type) {
       boolean doesShapeExist = false;
-      for (ReadOnlyIShape shape : this.listOfShapes) {
-        if (name.equals(shape.getShapeID())) {
+      for (IShape shape : this.listOfShapes) {
+        if (shape == null) {
+          break;
+        } else if (name.equals(shape.getShapeID())) {
           doesShapeExist = true;
           break;
         }
@@ -103,9 +105,10 @@ public class AnimatorModelImpl implements IAnimatorModel {
     }
 
     @Override
-    public AnimationBuilder<IAnimatorModel> addMotion(String name, int t1, int x1, int y1, int w1, int h1, int r1,
-                                      int g1, int b1, int t2, int x2, int y2, int w2, int h2,
-                                      int r2, int g2, int b2) {
+    public AnimationBuilder<IAnimatorModel> addMotion(String name, int t1, int x1, int y1, int w1,
+                                                      int h1, int r1, int g1, int b1, int t2,
+                                                      int x2, int y2, int w2, int h2, int r2,
+                                                      int g2, int b2) {
       boolean doesShapeExist = false;
       IShape currentShape = null;
       for (IShape shape : this.listOfShapes) {
