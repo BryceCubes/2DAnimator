3e890758227235c2bdd0920b4b8c26fc82821e7e
Bryce Russell-Benoit <brycerussellbenoit@gmail.com>
2019-06-16 08:49:06 -0400

    fuckin around pt 2

--- src/cs3500/animator/view/visual/AnimationView.java
+++ src/cs3500/animator/view/visual/AnimationView.java
@@ -1,11 +1,16 @@
 package cs3500.animator.view.visual;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 
 import javax.swing.*;
 
 import cs3500.animator.model.IAnimatorModel;
+import cs3500.animator.model.ReadOnlyIAnimatorModel;
+import cs3500.animator.model.shape.ReadOnlyIShape;
 import cs3500.animator.util.AnimationBuilder;
 import cs3500.animator.view.IAnimatorView;
 import cs3500.animator.view.SVGView;
@@ -13,7 +18,7 @@ import cs3500.animator.view.SVGView;
 public class AnimationView extends JFrame implements IAnimatorView {
   private AnimationPanel panel;
   private JScrollPane scrollPane;
-  private IAnimatorModel model;
+  private ReadOnlyIAnimatorModel model;
   private Timer timer;
   private int speed;
   private int tick;
@@ -21,9 +26,13 @@ public class AnimationView extends JFrame implements IAnimatorView {
 
   private AnimationView() {
     super();
-    //TODO: 1500?
-    timer = new Timer(1000 / this.speed, e -> animate());
-
+    timer = new Timer(1000 / this.speed, new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        ArrayList<ReadOnlyIShape> shapesToRender = model.returnShapesAtTick(tick++);
+      }
+    });
+    
     panel = new AnimationPanel();
     panel.setMinimumSize(new Dimension(500, 500));
     panel.setPreferredSize(new Dimension(2000, 2000));
