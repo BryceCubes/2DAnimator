dbcb6515c9fbfd3e24897cbd22af70799050ad04
Bryce Russell-Benoit <brycerussellbenoit@gmail.com>
2019-06-15 21:30:24 -0400

    cleaning up tests

--- test/cs3500/animator/model/AnimatorModelImplTest.java
+++ test/cs3500/animator/model/AnimatorModelImplTest.java
@@ -25,109 +25,11 @@ public class AnimatorModelImplTest {
   // circle with color changes combined with other types
   private IShape ethanCircle;
 
-  private IMotion fredMoveRight;
-  private IMotion fredMoveLeft;
-  private IMotion fredMoveUp;
-  private IMotion fredMoveDown;
-  private IMotion fredMoveUpRight;
-  private IMotion fredMoveDownLeft;
-
-  private IMotion amyGrowTall;
-  private IMotion amyGrowWide;
-  private IMotion amyScaleUp;
-  private IMotion amyShrinkHeight;
-  private IMotion amyShrinkWidth;
-  private IMotion amyScaleDown;
-
-  private IMotion ethanColorChanges;
-  private IMotion ethanAllChanges;
-
-  private IMotion fredBadStart;
-  private IMotion amyBadEnd;
-  private IMotion fredOverlap;
-  private IMotion ethanDisjoint;
-
   private IAnimatorModel model;
   private IAnimatorModel mtModel;
 
 
   private void setTest() {
-    // Rectangle movement
-    // orange square
-    frectangle = new AShape("Fred", ShapeType.RECTANGLE);
-    fredMoveRight = new ShapeMotion(frectangle, 10, 10, 5, 5, 255,
-            150, 10, 15, 10, 5, 5, 255, 150, 10,
-            0, 10);
-    fredMoveLeft = new ShapeMotion(frectangle, 15, 10, 5, 5, 255,
-            150, 10, 5, 10, 5, 5, 255, 150, 10,
-            10, 20);
-    fredMoveUp = new ShapeMotion(frectangle, 5, 10, 5, 5, 255,
-            150, 10, 5, 5, 5, 5, 255, 150, 10,
-            20, 30);
-    fredMoveDown = new ShapeMotion(frectangle, 5, 5, 5, 5, 255,
-            150, 10, 5, 15, 5, 5, 255, 150, 10,
-            30, 40);
-    fredMoveUpRight = new ShapeMotion(frectangle, 5, 15, 5, 5, 255,
-            150, 10, 15, 5, 5, 5, 255, 150, 10,
-            40, 50);
-    fredMoveDownLeft = new ShapeMotion(frectangle, 15, 5, 5, 5, 255,
-            150, 10, 10, 20, 5, 5, 255, 150, 10,
-            50, 60);
-
-    // Ellipse scaling
-    // tall blue Ellipse
-    amyOval = new AShape("Amy", ShapeType.ELLIPSE);
-    amyGrowTall = new ShapeMotion(amyOval, 50, 50, 10, 20, 0,
-            100, 255, 50, 50, 10, 25, 0, 100, 255,
-            0, 25);
-    amyGrowWide = new ShapeMotion(amyOval, 50, 50, 10, 25, 0,
-            100, 255, 50, 50, 20, 25, 0, 100, 255,
-            25, 50);
-    amyScaleUp = new ShapeMotion(amyOval, 50, 50, 20, 25, 0,
-            100, 255, 50, 50, 30, 35, 0, 100, 255,
-            50, 75);
-    amyShrinkHeight = new ShapeMotion(amyOval, 50, 50, 30, 35, 0,
-            100, 255, 50, 50, 30, 30, 0, 100, 255,
-            75, 100);
-    amyShrinkWidth = new ShapeMotion(amyOval, 50, 50, 30, 30, 0,
-            100, 255, 50, 50, 20, 30, 0, 100, 255,
-            100, 125);
-    amyScaleDown = new ShapeMotion(amyOval, 50, 50, 20, 30, 0,
-            100, 255, 50, 50, 10, 20, 0, 100, 255,
-            125, 150);
-
-    // Circle changing color
-    // starting light purple circle
-    ethanCircle = new AShape("Ethan", ShapeType.ELLIPSE);
-
-    // purple to green circle
-    ethanColorChanges = new ShapeMotion(ethanCircle, 25, 25, 15, 15, 180,
-            120, 230, 25, 25, 15, 15, 120, 180, 95,
-            0, 30);
-
-    // green to purple circle with movement up and to the right as well as expanding unevenly
-    ethanAllChanges = new ShapeMotion(ethanCircle, 25, 25, 15, 15, 120,
-            180, 95, 40, 15, 20, 30, 180, 120, 230,
-            30, 60);
-
-    // ERROR CHECKING MOVES
-
-    fredBadStart = new ShapeMotion(frectangle, 15, 10, 5, 5, 255,
-            150, 10, 5, 10, 5, 5, 255, 150, 10,
-            9, 20);
-
-    amyBadEnd = new ShapeMotion(amyOval, 50, 50, 10, 25, 0,
-            100, 255, 50, 50, 20, 25, 0, 100, 255,
-            25, 51);
-
-    fredOverlap = new ShapeMotion(frectangle, 15, 10, 5, 5, 255,
-            150, 10, 5, 10, 5, 5, 255, 150, 10,
-            5, 25);
-
-    ethanDisjoint = new ShapeMotion(ethanCircle, 26, 25, 15, 15, 120,
-            180, 95, 40, 15, 20, 30, 180, 120, 230,
-            30, 60);
-
 
     // Making the model and adding all shapes and motions
     model = new AnimatorModelImpl.Builder().declareShape("Fred", "RecTanGle")
