cd288368d071551ca5c2427574e9d458cbcbc089
Bryce Russell-Benoit <brycerussellbenoit@gmail.com>
2019-06-16 18:07:27 -0400

    :(

--- src/cs3500/animator/view/visual/IAnimationPanel.java
+++ src/cs3500/animator/view/visual/IAnimationPanel.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import cs3500.animator.model.shape.ReadOnlyIShape;
 
+/**
+ * Represents a general animation panel.
+ */
 public interface IAnimationPanel {
   void draw(List<ReadOnlyIShape> shapes);
 }
