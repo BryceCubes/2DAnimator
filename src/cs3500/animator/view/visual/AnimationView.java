ca95d6452be7207bf6b698ee5be82f56ac8c6104
Bryce Russell-Benoit <brycerussellbenoit@gmail.com>
2019-06-16 09:07:46 -0400

    fixed bad swing imports

--- src/cs3500/animator/view/visual/AnimationView.java
+++ src/cs3500/animator/view/visual/AnimationView.java
@@ -5,8 +5,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.Timer;
 
 import cs3500.animator.model.IAnimatorModel;
 import cs3500.animator.model.ReadOnlyIAnimatorModel;
