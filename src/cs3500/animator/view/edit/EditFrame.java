0accaa78cecadeebfcfee31d0201938e76cb64e2
Bryce Russell-Benoit <brycerussellbenoit@gmail.com>
2019-06-19 12:14:59 -0400

    smol

--- src/cs3500/animator/view/edit/EditFrame.java
+++ src/cs3500/animator/view/edit/EditFrame.java
@@ -5,10 +5,19 @@ import javax.swing.*;
 import cs3500.animator.view.IAnimatorView;
 
 public class EditFrame extends JFrame implements IAnimatorView {
+  private JPanel mainPanel;
+  private JScrollPane mainScroll;
+  private JButton button;
+
+  private JLabel animationPreview;
+
 
 
   private EditFrame() {
     super();
+    setTitle("Animation Editor");
+    setSize(500, 500);
+
   }
 
   @Override
